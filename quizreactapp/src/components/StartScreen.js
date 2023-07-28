b602c25afe39cb4ddf45764e70c58d85118c6cb0
Sean Pivin <seanpivin@gmail.com>
2023-07-28 11:07:47 +0300

    Added Pic

--- quizreactapp/src/components/StartScreen.js
+++ quizreactapp/src/components/StartScreen.js
@@ -1,8 +1,8 @@
 function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to the React Quiz App</h2>
-      <p>{numQuestions} questions to test your React mastery</p>
+      <h2>Welcome to the React Quiz</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
